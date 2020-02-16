198a08625e0deddee2094592d08ad494fdd66709
mkorablyov <mkorablyov@gmail.com>
2020-02-16 16:42:32 -0500

    added featuralizers

--- example_3d.py
+++ example_3d.py
@@ -31,6 +31,77 @@ def build_mol(smiles=None, num_conf=1, minimize=False, noh=True, charges=True):
     #return pd.DataFrame({"mol":[mol], "elem":[elem], "coord":[coord]})
 
 
+def mtable_feat(elem, charge=None, organic=False):
+    """
+
+    :param elem:
+    :param charge:
+    :return:
+    """
+    elem_feat = []
+    if not organic: elem_feat.append(np.asarray(np.floor_divide(elem + 2, 8), dtype=np.float32) / 2.)
+    elem_feat.append(np.asarray(np.remainder(elem + 2, 8), dtype=np.float32) / 4.)
+    elem_feat.append(np.ones(elem.shape, dtype=np.float32))
+    if charge is not None:
+        elem_feat.append(charge)
+    return np.stack(elem_feat,axis=1)
+
+
+def mpnn_feat(mol, ifcoord=True):
+    """mol rdkit molecule"""
+    atomtypes = {'H': 0, 'C': 1, 'N': 2, 'O': 3, 'F': 4}
+    bondtypes = {BT.SINGLE: 0, BT.DOUBLE: 1, BT.TRIPLE: 2, BT.AROMATIC: 3}
+
+    natm = len(mol.GetAtoms())
+    # featurize elements
+    atmfeat = pd.DataFrame(index=range(natm),columns=["type_idx", "atomic_number", "acceptor", "donor", "aromatic",
+                                                      "sp", "sp2", "sp3", "num_hs"])
+
+    # featurize
+    fdef_name = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
+    factory = ChemicalFeatures.BuildFeatureFactory(fdef_name)
+    for i,atom in enumerate(mol.GetAtoms()):
+        type_idx = atomtypes.get(atom.GetSymbol(),5)
+        atmfeat["type_idx"][i] = onehot([type_idx], num_classes=len(atomtypes) + 1)[0]
+        atmfeat["atomic_number"][i] = atom.GetAtomicNum()
+        atmfeat["aromatic"][i] = 1 if atom.GetIsAromatic() else 0
+        hybridization = atom.GetHybridization()
+        atmfeat["sp"][i] = 1 if hybridization == HybridizationType.SP else 0
+        atmfeat["sp2"][i] = 1 if hybridization == HybridizationType.SP2 else 0
+        atmfeat["sp3"][i] = 1 if hybridization == HybridizationType.SP3 else 0
+        atmfeat["num_hs"][i] = atom.GetTotalNumHs(includeNeighbors=True)
+
+    # get donors and acceptors
+    atmfeat["acceptor"].values[:] = 0
+    atmfeat["donor"].values[:] = 0
+    feats = factory.GetFeaturesForMol(mol)
+    for j in range(0, len(feats)):
+         if feats[j].GetFamily() == 'Donor':
+             node_list = feats[j].GetAtomIds()
+             for k in node_list:
+                 atmfeat["donor"][k] = 1
+         elif feats[j].GetFamily() == 'Acceptor':
+             node_list = feats[j].GetAtomIds()
+             for k in node_list:
+                 atmfeat["acceptor"][k] = 1
+    # get coord
+    if ifcoord:
+        coord = np.asarray([mol.GetConformer(0).GetAtomPosition(j) for j in range(natm)])
+    else:
+        coord = None
+    # get bonds and bond features
+    bond = np.asarray([[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()] for bond in mol.GetBonds()])
+    bondfeat = [bondtypes[bond.GetBondType()] for bond in mol.GetBonds()]
+    bondfeat = onehot(bondfeat,num_classes=len(bondtypes))
+
+    # convert atmfeat to numpy matrix
+    #if not panda_fmt:
+    #    type_idx = np.stack(atmfeat["type_idx"].values,axis=0)
+    #    atmfeat = atmfeat[["atomic_number", "acceptor", "donor", "aromatic", "sp", "sp2", "sp3","num_hs"]]
+    #    atmfeat = np.concatenate([type_idx, atmfeat.to_numpy(dtype=np.int)],axis=1)
+    return atmfeat, coord, bond, bondfeat
+
+
 if __name__ == "__main__":
     num_mols = 1000
     smis = df["smi"].to_list()[:num_mols]
